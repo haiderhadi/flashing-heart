238dbf59c3a10125ef8a63350d8f35f654e1f43a
haiderhadi <haider-81@live.com>
2024-07-01 00:55:50 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showIcon(IconNames.Angry)
+    basic.showIcon(IconNames.Target)
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.Scissors)
+})
